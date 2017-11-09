bb95aa9219e4bf97a335fa4b69cd8cf438d7f05e
Robyn-Dale Samuda <robyn@yuraki.com>
2017-11-08 18:51:25 -0600

    Test liquid parser

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import Liquid from 'liquid-node';
 
 class App extends Component {
-  render() {
+  outputLiquid = () => {
     let engine = new Liquid.Engine();
     engine
     .parse("Hello {{name}}")
-    .then((template) => { template.render({ name: "Robyn"})})
+    .then((template) => { return template.render({ name: "Robyn"})})
     .then((result) => { console.log(result)});
+  }
 
+  render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -20,6 +22,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.outputLiquid}>Do Liquid</button>
       </div>
     );
   }
