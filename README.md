b0c85fc880123fd6750548db437b1211cfb6d4c6
Robyn-Dale Samuda <robyn@yuraki.com>
2017-11-22 15:57:55 -0600

    Create navbar

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Liquid Playground
 
 ### Todo
+- Explore using trix editor - https://github.com/basecamp/trix
 - Better error handling and feedback
 - Store any used variables in browser storage
 - Install Hotjar
--- src/Components/PageHeader/index.js
+++ src/Components/PageHeader/index.js
@@ -3,7 +3,29 @@ import './PageHeader.css';
 
 const PageHeader = () => {
   return (
-    <div></div>
+    <div className="container PageHeader">
+      <nav className="navbar" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://www.github.com">
+            <h3 className="has-text-weight-bold is-size-3 has-text-dark">Liquid Playground</h3>
+          </a>
+
+          <button className="button navbar-burger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+
+        <a
+          className="navbar-item navbar-end button is-info"
+          href="https://github.com/samudary/liquid-playground"
+          target="_blank"
+          rel="noopener noreferrer">
+            Project on Github
+        </a>
+      </nav>
+    </div>
   );
 }
 
